f7ed33123c8d44904a84b6b2837eac16a56f7795
Swich <andruha313@gmail.com>
2025-06-15 00:35:51 +0300

    created monorepo, added TRPS client to webapp, added types check scripts for backend and webapp

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,21 @@
-console.log('Hello from the backend!')
+import express from 'express'
+import * as trpcExpress from '@trpc/server/adapters/express'
+import { trpcRouter } from './trpc'
+import cors from 'cors'
+
+const app = express()
+const port = 3000
+
+app.use(cors())
+
+app.get('/ping', (req, res) => {
+  res.json('pong')
+})
+
+app.use('/trpc', trpcExpress.createExpressMiddleware({
+  router: trpcRouter,
+}))
+
+app.listen(port, () => {
+  console.info(`Example app listening on port http://localhost:${port}`)
+})
