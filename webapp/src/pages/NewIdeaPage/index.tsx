2c33e027ae98a101c9c3f0641774ebfcab67c333
Swich <andruha313@gmail.com>
2025-06-22 22:27:50 +0300

    Added form to NewIdeaPage

--- webapp/src/pages/NewIdeaPage/index.tsx
+++ webapp/src/pages/NewIdeaPage/index.tsx
@@ -1,10 +1,31 @@
-import styles from './index.module.scss'
+import { useState } from 'react'
+import { Segment } from '../../components/Segment'
+import { Input } from '../../components/Input'
+import { Textarea } from '../../components/Textarea'
 
 export const NewIdeaPage = () => {
+  const [state, setState] = useState({
+    name: '',
+    nick: '',
+    description: '',
+    text: '',
+  })
+
   return (
-    <div>
-      <h1 className={styles.title}>New Idea Page</h1>
-      <p>This is where users can submit new ideas.</p>
-    </div>
+    <Segment title="New Idea">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          console.info('Submitted', state)
+        }}
+      >
+        <Input name="name" label="Name" state={state} setState={setState} />
+        <Input name="nick" label="Nick" state={state} setState={setState} />
+        <Input name="description" label="Description" state={state} setState={setState} />
+        <Textarea name="text" label="Text" state={state} setState={setState} />
+
+        <button type="submit">Create Idea</button>
+      </form>
+    </Segment>
   )
 }
